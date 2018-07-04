7476efd9e09b0a932e6a050e3772952cfcc36eb4
sakib-hossain-zim <sakib_hossain@DESKTOP-VNS6AG8.localdomain>
2018-07-04 15:46:40 -0700

    Worked on Tables in faq.js

--- particles/react/src/components/Pages/Faq.js
+++ particles/react/src/components/Pages/Faq.js
@@ -12,19 +12,26 @@ export default class Invoice extends Component {
                                 <h1>Frequently Asked Questions</h1>
                             </div>
 
-                                <table>
-                                  <thead>
-                                    <tr>
-                                      <th>Question</th>
-                                      <th>Answer</th>
-                                    </tr>
-                                  </thead>
 
-                                  <tbody>
-                                    
+                  <table class="table table-bordered table-striped" id="pastvote_table">
+                 <thead class="thead-dark">
+                 <tr>
+                 <th>Identifier</th>
+                  <th>Vote Decision</th>
+                  <th>Transaction Id</th>
+                  <th>Time Stamp</th>
+                  <th>New</th>
+                  <th>Old</th>
+                  <th>Auth</th>
+                  <th>Time Stamp</th>
+                  <th>New</th>
+                  <th>Old</th>
 
-                                  </tbody>
-                                </table>
+
+                 </tr>
+                     </thead>
+             </table>
+
 
                   </div>
               </div>
